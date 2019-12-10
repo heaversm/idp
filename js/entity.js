89004a7b4abe7801ddbcf394d76b7f9d9427361e
Mike Heavers <mheavers@gmail.com>
2019-12-10 14:17:30 -0800

    initial commit

---
+++ js/entity.js
@@ -0,0 +1,119 @@
+(function(){
+
+  var button = document.getElementById('cn-button'),
+  wrapper = document.getElementById('cn-wrapper'),
+  overlay = document.getElementById('cn-overlay');
+
+  $wrapper = $('.cn-wrapper');
+  $contentWrapper = $('.content-wrapper');
+  $petals = $('.petal');
+
+  var $arrow = $('.arrow');
+  var curIndex = 0;
+  var totalIndex = 8;
+  var rotateAmt = 0;
+
+  //open and close menu when the button is clicked
+  var open = false;
+  button.addEventListener('click', handler, false);
+  wrapper.addEventListener('click', cnhandle, false);
+  $arrow.on('click',onArrowClick);
+  $petals.on('click',onPetalClick);
+  $('.select-instance').on('click',onInstanceClick);
+
+  initializeFirstItem();
+
+  function initializeFirstItem(){
+      $(button).trigger('click');
+      setTimeout(function(){
+          $('.arrow').addClass('active');
+      },1500);
+  }
+
+
+  function onArrowClick(){
+      $thisArrow = $(this);
+      var isLeft = $thisArrow.hasClass('left');
+      if (isLeft){
+          if (curIndex > 0){
+              curIndex--;
+          } else {
+              curIndex = totalIndex;
+          }
+          rotateAmt -= 40;
+      } else {
+          if (curIndex < totalIndex){
+              curIndex++;
+          } else {
+              curIndex = 0;
+          }
+          rotateAmt+=40;
+      }
+
+      $wrapper.css({'transform': 'translate(-50%,-50%) rotate(' + rotateAmt + 'deg'});
+          displayContent();
+  }
+
+  function onPetalClick(){
+      var $thisPetal = $(this);
+      curIndex = $thisPetal.index();
+      var rotateIndex = curIndex-2; //MH - 0 index is at the left position , so two turns will get it to the top, thus the -2
+
+      rotateAmt = -rotateIndex*40;
+      console.log(curIndex,rotateAmt);
+
+      $wrapper.css({'transform': 'translate(-50%,-50%) rotate(' + rotateAmt + 'deg'});
+          displayContent();
+
+  }
+
+  function onInstanceClick(e){
+      e.stopPropagation();
+      $contentWrapper.removeClass('active');
+      //$(button).trigger('click');
+      closeNav();
+  }
+
+  function cnhandle(e){
+      e.stopPropagation();
+  }
+
+  function handler(e){
+
+      e.stopPropagation();//so that it doesn't trigger click event on document
+
+      if(!open){
+          openNav();
+      }
+      else{
+          closeNav();
+      }
+  }
+  function openNav(){
+      open = true;
+      classie.add(overlay, 'on-overlay');
+      classie.add(wrapper, 'opened-nav');
+      $(button).addClass('active');
+      setTimeout(displayContent,1200);
+  }
+  function closeNav(){
+      open = false;
+
+      classie.remove(overlay, 'on-overlay');
+      classie.remove(wrapper, 'opened-nav');
+      $(button).removeClass('active');
+      $arrow.removeClass('active');
+  }
+
+  function displayContent(){
+      var $curPetal = $('.petal').eq(curIndex);
+      var $curSpan = $curPetal.find('span');
+      var curText = $curSpan.text();
+      $contentWrapper.find('.title').text(curText);
+      $contentWrapper.addClass('active');
+
+  }
+  //document.addEventListener('click', closeNav);
+
+})();
+
---
+++ js/home.js
@@ -0,0 +1,98 @@
+
+var width = window.innerWidth,
+height = window.innerHeight,
+numCircles = 100,
+minCircleSize = 4,
+maxCircleSize = 12,
+circleDark = d3.rgb(22, 220, 171),
+circleLight = d3.rgb(10, 112, 88),
+circleCollideRadius = 16,
+circleCharge = -2000; //repulsion or attraction of circles from center
+
+var nodes = d3.range(100).map(function() { return {radius: Math.random() * maxCircleSize
+  + minCircleSize}; }),
+root = nodes[0];
+root.radius = 0;
+root.fixed = true;
+root.px = Math.floor(width/2);
+root.py = Math.floor(height/2);
+
+
+
+var force = d3.layout.force()
+.gravity(0.03)
+.charge(function(d, i) { return i ? 0 : circleCharge; })
+.nodes(nodes)
+.size([width, height]);
+
+force.start();
+
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height);
+
+svg.selectAll("circle")
+.data(nodes.slice(1))
+.enter().append("circle")
+.attr("r", function(d) { return d.radius; })
+.style({
+  //"fill" :  function(d, i) { return color; },
+  "fill": function(d,i){ return d3.interpolate(circleDark,circleLight)(Math.random())},
+  //"opacity" : function(d,i){ return Math.random(); } //TODO: do this with color only, not opacity
+  //"opacity": 1,
+});
+
+force.on("tick", function(e) {
+var q = d3.geom.quadtree(nodes),
+  i = 0,
+  n = nodes.length;
+
+while (++i < n) q.visit(collide(nodes[i]));
+
+svg.selectAll("circle")
+  .attr("cx", function(d) { return d.x; })
+  .attr("cy", function(d) { return d.y; });
+});
+
+svg.on("mousemove", cursorMove);
+svg.on("touchmove", cursorMove);
+
+svg.on("mouseleave", cursorEnd);
+svg.on("touchend", cursorEnd);
+
+function cursorMove(){
+var p1 = d3.mouse(this);
+root.px = p1[0];
+root.py = p1[1];
+force.resume();
+}
+
+function cursorEnd(){
+root.px = Math.floor(width/2);
+root.py = Math.floor(height/2);
+force.resume();
+}
+
+function collide(node) {
+var r = node.radius + circleCollideRadius,
+  nx1 = node.x - r,
+  nx2 = node.x + r,
+  ny1 = node.y - r,
+  ny2 = node.y + r;
+return function(quad, x1, y1, x2, y2) {
+if (quad.point && (quad.point !== node)) {
+  var x = node.x - quad.point.x,
+      y = node.y - quad.point.y,
+      l = Math.sqrt(x * x + y * y),
+      r = node.radius + quad.point.radius;
+  if (l < r) {
+    l = (l - r) / l * .5;
+    node.x -= x *= l;
+    node.y -= y *= l;
+    quad.point.x += x;
+    quad.point.y += y;
+  }
+}
+return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+};
+}
